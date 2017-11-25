0e4bdd69b0fc8456cfca8755d15292f84d08b71e
mrswonka <kalakolcheg96@mail.ru>
2017-11-25 20:17:07 +0500

    Комментарий

--- src/main/java/com/usatu/servlets/MainServlet.java
+++ src/main/java/com/usatu/servlets/MainServlet.java
@@ -9,7 +9,7 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class MainServlet extends HttpServlet {
-
+//новый комментарий
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req,resp);
